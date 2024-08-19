5db2bae8cf4a8173e88b9bfd80d2750980ecd42b
Mark Mwima <markmwimar@gmail.com>
2024-08-19 17:29:10 +0300

    compile method

--- src/main/java/com/spring1/Laptop.java
+++ src/main/java/com/spring1/Laptop.java
@@ -1,7 +1,10 @@
 package com.spring1;
 
 public class Laptop {
-//    public Laptop(){
-//        System.out.println("new Laptop object");
-//    }
+    public Laptop(){
+        System.out.println("new Laptop object");
+    }
+    public void compile(){
+        System.out.println("Compiling...");
+    }
 }
