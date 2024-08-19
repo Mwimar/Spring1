3a95083f2e64b71f553e421b55fb68d64985932e
Mark Mwima <markmwimar@gmail.com>
2024-08-19 17:14:01 +0300

    age getter and setter

--- src/main/java/com/spring1/Alien.java
+++ src/main/java/com/spring1/Alien.java
@@ -1,7 +1,15 @@
 package com.spring1;
 
 public class Alien {
-    int age =25;
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private int age =25;
     public Alien(){
         System.out.println("Alien object");
     }
--- src/main/java/com/spring1/Main.java
+++ src/main/java/com/spring1/Main.java
@@ -7,10 +7,10 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         Alien obj1 = (Alien) context.getBean("alien1");
-        obj1.age = 30;
-        System.out.println(obj1.age);
+//        obj1.age = 30;
+//        System.out.println(obj1.age);
 
         Alien obj2 = (Alien) context.getBean("alien1");
-        System.out.println(obj2.age);
+//        System.out.println(obj2.age);
     }
 }
