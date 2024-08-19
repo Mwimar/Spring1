5a00836a1aba378d6213276f35c6d8e96ef3b598
Mark Mwima <markmwimar@gmail.com>
2024-08-19 15:30:32 +0300

    laptop class

--- src/main/java/com/spring1/Main.java
+++ src/main/java/com/spring1/Main.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj = (Alien) context.getBean("alien");
-        obj.code();
+//        Alien obj = (Alien) context.getBean("alien");
+//        obj.code();
     }
 }
