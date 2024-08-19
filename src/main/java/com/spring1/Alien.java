c22f3f92b313a125271646ff476d995b992dce90
Mark Mwima <markmwimar@gmail.com>
2024-08-19 15:26:42 +0300

    alien constructor

--- src/main/java/com/spring1/Alien.java
+++ src/main/java/com/spring1/Alien.java
@@ -1,6 +1,9 @@
 package com.spring1;
 
 public class Alien {
+    public Alien(){
+        System.out.println("Alien object");
+    }
     public void code(){
         System.out.println("Coding...");
     }
