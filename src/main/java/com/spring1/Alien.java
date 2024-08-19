9a812c252b04bc1919aeed3d8316272104c06eb9
Mark Mwima <markmwimar@gmail.com>
2024-08-19 15:43:02 +0300

    singleton scope

--- src/main/java/com/spring1/Alien.java
+++ src/main/java/com/spring1/Alien.java
@@ -1,6 +1,7 @@
 package com.spring1;
 
 public class Alien {
+    int age =25;
     public Alien(){
         System.out.println("Alien object");
     }
--- src/main/java/com/spring1/Main.java
+++ src/main/java/com/spring1/Main.java
@@ -6,7 +6,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-//        Alien obj = (Alien) context.getBean("alien");
-//        obj.code();
+        Alien obj1 = (Alien) context.getBean("alien1");
+        obj1.age = 30;
+        System.out.println(obj1.age);
+
+        Alien obj2 = (Alien) context.getBean("alien1");
+        System.out.println(obj2.age);
     }
 }
