4cfe0cc0f7cdb3824b7f5954a9221b738db114e3
Mark Mwima <markmwimar@gmail.com>
2024-08-21 19:52:02 +0300

    app context using annotationconfig

--- src/main/java/com/spring1/Main.java
+++ src/main/java/com/spring1/Main.java
@@ -1,15 +1,33 @@
 package com.spring1;
 
+import com.spring1.config.AppConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj = (Alien) context.getBean("alien1", Alien.class);
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//        Alien obj = (Alien) context.getBean("alien1", Alien.class);
 //      obj.setAge(30);
-obj.code();
-    System.out.println(obj.getAge());
+//obj.code();
+//    System.out.println(obj.getAge());
 //Computer com = context.getBean(Computer.class);
 //Desktop obj1 = context.getBean( Desktop.class);
     }
