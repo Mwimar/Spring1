b2760fbb3b45f9d1e7156e017ff0e094ecfc6d22
Mark Mwima <markmwimar@gmail.com>
2024-08-21 19:58:04 +0300

    creating bean

--- src/main/java/com/spring1/config/AppConfig.java
+++ src/main/java/com/spring1/config/AppConfig.java
@@ -1,7 +1,15 @@
 package com.spring1.config;
 
+import com.spring1.Desktop;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public Desktop desktop(){
+        return new Desktop();
+    };
+
 }
