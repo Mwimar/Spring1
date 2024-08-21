addadebb4724e5b76b584a9ad75ea9556fae7e8a
Mark Mwima <markmwimar@gmail.com>
2024-08-21 16:51:18 +0300

    Desktop constructor

--- src/main/java/com/spring1/Desktop.java
+++ src/main/java/com/spring1/Desktop.java
@@ -1,6 +1,12 @@
 package com.spring1;
 
 public class Desktop implements Computer{
+
+    public Desktop(){
+        System.out.println("desktop object created");
+    }
+
+
     @Override
     public void compile(){
         System.out.println("Compiling using Desktop...");
